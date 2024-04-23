b3dec662d93df84ab4fd7ccb2ba9a66d6a80deba
Ben Wilkens <bwilkens@wilkensltd.com>
2024-04-23 15:53:46 -0400

    working

--- tasks.py
+++ tasks.py
@@ -2,6 +2,9 @@ from robocorp.tasks import task
 from robocorp import browser
 from RPA.HTTP import HTTP
 from RPA.Tables import Tables
+from RPA.PDF import PDF
+from RPA.Archive import Archive
+
 
 @task
 def order_robots_from_RobotSpareBin():
@@ -15,6 +18,7 @@ def order_robots_from_RobotSpareBin():
     open_robot_order_website()
     download_order_file()
     loop_and_process_orders()
+    archive_receipts()
     # fill_the_form()
 
 
@@ -38,22 +42,47 @@ def get_orders():
     # Function code here
     library = Tables()
     orders = library.read_table_from_csv(
-        "orders.csv", columns=["Order","number","Head","Body","Legs","Address"]
+        "orders.csv", columns=["Order number","Head","Body","Legs","Address"]
     )
     return orders
 
 def fill_the_form(order):
     page = browser.page()
-    
+    heads = {
+    "1" : "Roll-a-thor head",
+    "2" : "Peanut crusher head",
+    "3" : "D.A.V.E head",
+    "4" : "Andy Roid head",
+    "5" : "Spanner mate head",
+    "6" : "Drillbit 2000 head"
+    }
+    head = order["Head"]
+    page.select_option("#head", heads.get(head))
     # Order number,Head,Body,Legs,Address
+    
     # Choose Head from dropdown
+    heads = {
+    "1" : "Roll-a-thor head",
+    "2" : "Peanut crusher head",
+    "3" : "D.A.V.E head",
+    "4" : "Andy Roid head",
+    "5" : "Spanner mate head",
+    "6" : "Drillbit 2000 head"
+    }
+    head = order["Head"]
+    page.select_option("#head", heads.get(head))
     
     # Click Body radion button
+    # default to 0 but replace using formatted string with Body option from each order
+    page.click('//*[@id="root"]/div/div[1]/div/div[1]/form/div[2]/div/div[{0}]/label'.format(order["Body"]))
+    
     # Enter legs
-    page.fill("xpath=/html/body/div[1]/div/div[1]/div/div[1]/form/div[3]/input", order['Legs'])
+    # page.fill("input[placeholder='Enter the part number for the legs']", order['Legs'])
+    page.fill("input[placeholder='Enter the part number for the legs']", order["Legs"])
+
     # Enter shipping address
-    page.fill("xpath=/html/body/div[1]/div/div[1]/div/div[1]/form/div[4]/inputt", order['Address'])
-    page.screenshot(path="sreenshots/order_"+order['Order'])
+    page.fill("xpath=/html/body/div[1]/div/div[1]/div/div[1]/form/div[4]/input", order['Address'])
+    
 
 
 def close_annoying_modal():
@@ -61,24 +90,55 @@ def close_annoying_modal():
     page.click("button:text('OK')")
 
 def loop_and_process_orders():
+    page = browser.page()
     orders = get_orders()
     for order in orders:
         fill_the_form(order)
+        # take a screenshot of the preview
+        page.click("button:text('Preview')")
+        page.wait_for_selector(selector="#robot-preview-image")
+        
+        # Submit order
+        error_occured = True
 
-        print(order)
+        while error_occured:
+            page.click("button:text('Order')")
+            error_occured = page.locator("//div[@class= 'alert alert-danger']").is_visible()
+            if not error_occured:
+                break
+        
+        # save PDF of receipt
+        store_receipt_as_pdf(order["Order number"])
+        page.click("#order-another")
+        close_annoying_modal()
 
 
-"""
-from RPA.Tables import Tables
+def store_receipt_as_pdf(order_number):
+    """Store each receipt as a PDf"""
+    page = browser.page()
+    receipt_html = page.locator("#receipt").inner_html()
+    pdf = PDF()
+    receipt_path = "receipts/order_{0}.pdf".format(order_number)
+    pdf.html_to_pdf(receipt_html, receipt_path)
+    # Embed screenshot in each PDF
+    screenshot_robot_preview(order_number)
+    embed_screenshot_in_receipt_pdf("screenshots/order_"+order_number+".png", receipt_path)
 
-library = Tables()
-orders = library.read_table_from_csv(
-    "orders.csv", columns=["name", "mail", "product"]
-)
 
-customers = library.group_table_by_column(rows, "mail")
-for customer in customers:
-    for order in customer:
-        add_cart(order)
-    make_order()
-"""
+
+
+def screenshot_robot_preview(order_number):
+    """Screenshot the robot preview"""
+    page = browser.page()
+    page.locator(selector="#robot-preview-image").screenshot(path="screenshots/order_"+order_number+".png")
+
+
+def embed_screenshot_in_receipt_pdf(screenshot, receipt):
+    pdf = PDF()
+    pdf.add_files_to_pdf( files=[screenshot], target_document=receipt, append=True)
+
+
+def archive_receipts():
+    """Create Zip Archive of all reciepts"""
+    lib = Archive()
+    lib.archive_folder_with_zip("receipts","receipts.zip",recursive=True)
